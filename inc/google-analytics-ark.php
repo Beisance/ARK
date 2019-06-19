79ca6d5c4f144aea32474a2250690e8906bf2602
Olanrewaju Jeph <olanrewaju.jeph@beisance.com>
2019-06-19 13:49:03 +0400

    ARK-2-X

--- inc/google-analytics-ark.php
+++ inc/google-analytics-ark.php
@@ -3,13 +3,22 @@
  * The Sidebar containing the primary and secondary widget areas.
  * @package WordPress
  * @subpackage Beisance|Intelix Custom Theme For Beisance|ARK
- * Account: ***
+ * Account: web.services@beisance.com
  */
 
 ?>
 
 <div id="google-analytics"><!-- start #google-analytics-->
 	
-	<!-- place your own google analytics tracking code here -->
+	<!-- Specific ARK site tracking -->
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-33249853-4"></script>
+	<script>
+	window.dataLayer = window.dataLayer || [];
+	function gtag(){dataLayer.push(arguments);}
+	gtag('js', new Date());
+
+	gtag('config', 'UA-33249853-4');
+	</script>
 	
 </div><!-- end #google-analytics-->
