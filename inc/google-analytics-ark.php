c9f5ef2a056c5d9b4aebcfde38c386227d44adc4
O.J <Olanrewaju@users.noreply.github.com>
2019-06-19 14:29:08 +0400

    Add files via upload

---
+++ inc/google-analytics-ark.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * The Sidebar containing the primary and secondary widget areas.
+ * @package WordPress
+ * @subpackage Beisance|Intelix Custom Theme For Beisance|ARK
+ * Account: ***
+ */
+
+?>
+
+<div id="google-analytics"><!-- start #google-analytics-->
+	
+	<!-- Enter your own Google Analytics tracking code here -->
+	
+</div><!-- end #google-analytics-->
