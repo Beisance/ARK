845c2bb2237cf4be7b65bbbfa7a503559769dad4
O.J <Olanrewaju@users.noreply.github.com>
2019-06-19 13:51:17 +0400

    Update google-analytics-ark.php

--- inc/google-analytics-ark.php
+++ inc/google-analytics-ark.php
@@ -3,22 +3,13 @@
  * The Sidebar containing the primary and secondary widget areas.
  * @package WordPress
  * @subpackage Beisance|Intelix Custom Theme For Beisance|ARK
- * Account: web.services@beisance.com
+ * Account: ***
  */
 
 ?>
 
 <div id="google-analytics"><!-- start #google-analytics-->
 	
-	<!-- Specific ARK site tracking -->
-	<!-- Global site tag (gtag.js) - Google Analytics -->
-	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-33249853-4"></script>
-	<script>
-	window.dataLayer = window.dataLayer || [];
-	function gtag(){dataLayer.push(arguments);}
-	gtag('js', new Date());
-
-	gtag('config', 'UA-33249853-4');
-	</script>
+	<!-- Place your own Google Analytics tracking code here -->
 	
 </div><!-- end #google-analytics-->
